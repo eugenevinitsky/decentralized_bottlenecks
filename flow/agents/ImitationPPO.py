Fixed bug where gradients werent actually being computed

flow/agents/ImitationPPO.py
@@ -2,6 +2,8 @@ from ray.rllib.agents.ppo.ppo import PPOTrainer
 from ray.rllib.agents.ppo.ppo_policy import PPOTFPolicy
 from ray.rllib.agents.ppo.ppo_policy import LearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin, ValueNetworkMixin
 from ray.rllib.utils.annotations import override, DeveloperAPI
+from ray.rllib.utils.explained_variance import explained_variance
+from ray.rllib.evaluation.postprocessing import Postprocessing
 from ray.rllib.policy.policy import Policy
 from ray.rllib.models.model import restore_original_dimensions
 from ray.rllib.agents.ppo.ppo_policy import ppo_surrogate_loss, kl_and_loss_stats
@@ -11,7 +13,9 @@ import tensorflow as tf
 def imitation_loss(policy, model, dist_class, train_batch):
     original_space = restore_original_dimensions(train_batch['obs'], model.obs_space)
     expert_tensor = original_space['expert_action']
-    policy_actions = train_batch['actions']
+    logits, state = model.from_batch(train_batch)
+    action_dist = dist_class(logits, model)
+    policy_actions = action_dist.sample()
     imitation_loss = tf.reduce_mean(tf.squared_difference(policy_actions, expert_tensor))
     return imitation_loss
 
@@ -28,11 +32,12 @@ class ImitationLearningRateSchedule(object):
     """Mixin for TFPolicy that adds a learning rate schedule."""
 
     @DeveloperAPI
-    def __init__(self, num_imitation_iters, imitation_weight):
+    def __init__(self, num_imitation_iters, imitation_weight, config):
         self.imitation_weight = tf.get_variable("imitation_weight", initializer=float(imitation_weight),
                                                 trainable=False, dtype=tf.float32)
         self.policy_weight = tf.get_variable("policy_weight", initializer=0.0, trainable=False,
                                              dtype=tf.float32)
+        self.start_kl_val = config["kl_coeff"]
         self.num_imitation_iters = num_imitation_iters
         self.curr_iter = 0
 
@@ -43,6 +48,9 @@ class ImitationLearningRateSchedule(object):
         if self.curr_iter > self.num_imitation_iters:
             self.imitation_weight.load(0.0, session=self._sess)
             self.policy_weight.load(1.0, session=self._sess)
+        else:
+            # we don't want the kl coefficient to go wild while we are imitating
+            self.kl_coeff.load(self.start_kl_val, session=self.get_session())
         self.curr_iter += 1
 
 
@@ -60,13 +68,24 @@ def setup_mixins(policy, obs_space, action_space, config):
                                   config["entropy_coeff_schedule"])
     LearningRateSchedule.__init__(policy, config["lr"], config["lr_schedule"])
     ImitationLearningRateSchedule.__init__(policy, config["model"]["custom_options"]["num_imitation_iters"],
-                                           config["model"]["custom_options"]["imitation_weight"])
+                                           config["model"]["custom_options"]["imitation_weight"], config)
+
+
+
+def grad_stats(policy, train_batch, grads):
+    return {
+        "grad_gnorm": tf.global_norm(grads),
+        "vf_explained_var": explained_variance(
+            train_batch[Postprocessing.VALUE_TARGETS],
+            policy.model.value_function()),
+    }
 
 
 ImitationPolicy = PPOTFPolicy.with_updates(
     name="ImitationPolicy",
     before_loss_init=setup_mixins,
     stats_fn=loss_state,
+    grad_stats_fn=grad_stats,
     loss_fn=new_ppo_surrogate_loss,
     mixins=[
         LearningRateSchedule, EntropyCoeffSchedule, KLCoeffMixin,

flow/multiagent_envs/multi_bottleneck_env.py
@@ -235,6 +235,11 @@ class MultiBottleneckEnv(MultiEnv, DesiredVelocityEnv):
         ]) / 50
         outflow = np.asarray(
             self.k.vehicle.get_outflow_rate(20 * self.sim_step) / 2000.0)
+        temp = np.concatenate((num_vehicles_list, num_rl_vehicles_list,
+                               mean_speed_norm, mean_rl_speed, [outflow],
+                               [self.inflow]))
+        if np.any(temp < 0):
+            import ipdb; ipdb.set_trace()
         return np.concatenate((num_vehicles_list, num_rl_vehicles_list,
                                mean_speed_norm, mean_rl_speed, [outflow],
                                [self.inflow]))
@@ -511,7 +516,9 @@ class MultiBottleneckImitationEnv(MultiBottleneckEnv):
         obs = super().observation_space
         # Extra keys are the current q value, whether the vehicle is stopped at the edge, and the
         # time since when we stopped
-        new_obs = Box(low=-3.0, high=3.0, shape=(obs.shape[0] + 3,), dtype=np.float32)
+        # TODO(@evinitsky) put this back
+        # new_obs = Box(low=-3.0, high=3.0, shape=(obs.shape[0] + 3,), dtype=np.float32)
+        new_obs = Box(low=-3.0, high=3.0, shape=(obs.shape[0],), dtype=np.float32)
         return Dict({"obs": new_obs, "expert_action": self.action_space})
 
     def reset(self, new_inflow_rate=None):
@@ -533,31 +540,37 @@ class MultiBottleneckImitationEnv(MultiBottleneckEnv):
                                             car_following_params=SumoCarFollowingParams())
 
         for key, value in state_dict.items():
-            if self.k.vehicle.get_edge(key)[0] is not ':':
-                _ = self.curr_rl_vehicles[key]['controller'].get_accel(self)
-                # if we are stopped we don't actually return an accel
-                if self.curr_rl_vehicles[key]['controller'].stop_set:
-                    accel = 0
-                else:
-                    self.idm_controller.veh_id = key
-                    accel = self.idm_controller.get_accel(self)
-            else:
-                accel = self.idm_controller.get_accel(self)
-            # check if we have come up to the front edge and stopped
-            if not self.curr_rl_vehicles[key]['is_stopped'] and self.curr_rl_vehicles[key]['controller'].is_waiting_to_go:
-                self.curr_rl_vehicles[key]['is_stopped'] = True
-                self.curr_rl_vehicles[key]['stop_time'] = self.time_counter
-            # we have exited the edge so update this
-            if self.curr_rl_vehicles[key]['is_stopped'] and not self.curr_rl_vehicles[key]['controller'].is_waiting_to_go:
-                self.curr_rl_vehicles[key]['is_stopped'] = False
-
+            # TODO(@evinitsky) put this back
+            # if self.k.vehicle.get_edge(key)[0] is not ':':
+            #     _ = self.curr_rl_vehicles[key]['controller'].get_accel(self)
+            #     # if we are stopped we don't actually return an accel
+            #     if self.curr_rl_vehicles[key]['controller'].stop_set:
+            #         accel = 0
+            #     else:
+            #         self.idm_controller.veh_id = key
+            #         accel = self.idm_controller.get_accel(self)
+            # else:
+            #     accel = self.idm_controller.get_accel(self)
+            # # check if we have come up to the front edge and stopped
+            # if not self.curr_rl_vehicles[key]['is_stopped'] and self.curr_rl_vehicles[key]['controller'].is_waiting_to_go:
+            #     self.curr_rl_vehicles[key]['is_stopped'] = True
+            #     self.curr_rl_vehicles[key]['stop_time'] = self.time_counter
+            # # we have exited the edge so update this
+            # if self.curr_rl_vehicles[key]['is_stopped'] and not self.curr_rl_vehicles[key]['controller'].is_waiting_to_go:
+            #     self.curr_rl_vehicles[key]['is_stopped'] = False
+            #
+            # accel = np.clip(accel, a_min=self.action_space.low, a_max=self.action_space.high)
+            # if not isinstance(accel, np.ndarray):
+            #     accel = np.array(accel)
+            # curr_vehicle = self.curr_rl_vehicles[key]['controller']
+            # state_dict[key] = {"obs": np.concatenate((value, [curr_vehicle.q / curr_vehicle.q_max,
+            #                                                   self.curr_rl_vehicles[key]['is_stopped'],
+            #                                                   (self.time_counter -
+            #                                                    self.curr_rl_vehicles[key]['stop_time'])/self.env_params.horizon])),
+            #                    "expert_action": accel}
+            self.idm_controller.veh_id = key
+            accel = self.idm_controller.get_accel(self)
             accel = np.clip(accel, a_min=self.action_space.low, a_max=self.action_space.high)
-            if not isinstance(accel, np.ndarray):
-                accel = np.array(accel)
-            curr_vehicle = self.curr_rl_vehicles[key]['controller']
-            state_dict[key] = {"obs": np.concatenate((value, [curr_vehicle.q / curr_vehicle.q_max,
-                                                              self.curr_rl_vehicles[key]['is_stopped'],
-                                                              (self.time_counter -
-                                                               self.curr_rl_vehicles[key]['stop_time'])/self.env_params.horizon])),
+            state_dict[key] = {"obs": value,
                                "expert_action": accel}
         return state_dict