Correct the sign in hard negative mining

flow/agents/ImitationPPO.py
@@ -31,11 +31,11 @@ def imitation_loss(policy, model, dist_class, train_batch):
             train_batch[Postprocessing.ADVANTAGES], dtype=tf.bool)
 
     if policy.config['model']['custom_options']["hard_negative_mining"]:
-        masked_logp = tf.boolean_mask(action_dist.logp(expert_tensor), mask)
+        # negative sign makes values that are really negative be selected in the top k
+        masked_logp = -tf.boolean_mask(action_dist.logp(expert_tensor), mask)
         top_loss, _ = tf.math.top_k(masked_logp,
                                     int(policy.config['sgd_minibatch_size'] / 10))  # todo make this an actual 10%
-        top_loss = tf.reduce_sum(top_loss)
-        imitation_loss = -tf.reduce_mean(top_loss)
+        imitation_loss = tf.reduce_mean(top_loss)
 
     else:
         # Since we are doing gradient descent, we flip the sign so that we are minimizing the negative log prob