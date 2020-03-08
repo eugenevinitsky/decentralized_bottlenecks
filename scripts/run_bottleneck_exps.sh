8ee2e23438907fecf3e905f3d622e2909434292c
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-07 16:21:29 -0800

    Second commit for 03/07 exps

--- scripts/run_bottleneck_exps.sh
+++ scripts/run_bottleneck_exps.sh
@@ -1126,16 +1126,30 @@
 #--multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10 --imitate" \
 #--start --stop --cluster-name ev_2pen_im_1 --tmux
 
+#####################################################################################################################################################
+#### 3/07/20 exps
+##ray exec ray_autoscale.yaml \
+##"python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py 0pen_i2400 --num_iters 450 --checkpoint_freq 50 --av_frac 0.4 \
+##--num_samples 1 --grid_search --n_cpus 12 --use_s3 --rollout_scale_factor 2.0 --horizon 200 --low_inflow 2400 --high_inflow 2400 --aggregate_info \
+##--multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10" \
+##--start --stop --cluster-name ev_2pen_1 --tmux
+##
+##ray exec ray_autoscale.yaml \
+##"python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py 0pen_i2400_cvf --num_iters 450 --checkpoint_freq 50 --av_frac 0.4 \
+##--num_samples 1 --grid_search --n_cpus 12 --use_s3 --rollout_scale_factor 2.0 --horizon 200 --low_inflow 2400 --high_inflow 2400 --aggregate_info \
+##--multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10 --centralized_vf" \
+##--start --stop --cluster-name ev_2pen_2 --tmux
+
 ####################################################################################################################################################
-## 3/06/20 exps
+## 3/07/20 exps
 ray exec ray_autoscale.yaml \
 "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py 0pen_i2400 --num_iters 450 --checkpoint_freq 50 --av_frac 0.4 \
 --num_samples 1 --grid_search --n_cpus 12 --use_s3 --rollout_scale_factor 2.0 --horizon 200 --low_inflow 2400 --high_inflow 2400 --aggregate_info \
---multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10" \
+--multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10 --terminal_reward" \
 --start --stop --cluster-name ev_2pen_1 --tmux
 
 ray exec ray_autoscale.yaml \
 "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py 0pen_i2400_cvf --num_iters 450 --checkpoint_freq 50 --av_frac 0.4 \
 --num_samples 1 --grid_search --n_cpus 12 --use_s3 --rollout_scale_factor 2.0 --horizon 200 --low_inflow 2400 --high_inflow 2400 --aggregate_info \
---multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10 --centralized_vf" \
+--multi_node --sim_step 0.5 --life_penalty 0.0 --create_inflow_graph --sims_per_step 10 --centralized_vf --max_num_agents 200 --terminal_reward" \
 --start --stop --cluster-name ev_2pen_2 --tmux
