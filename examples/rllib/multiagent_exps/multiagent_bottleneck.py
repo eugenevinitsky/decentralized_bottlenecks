8c28962e5b95f146ff85bacf39f9b2fff3950dbc
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-11 19:33:15 -0700

    Attempting to make memory work

--- examples/rllib/multiagent_exps/multiagent_bottleneck.py
+++ examples/rllib/multiagent_exps/multiagent_bottleneck.py
@@ -325,14 +325,14 @@ def setup_exps(args):
         if not args.centralized_vf:
             config['sgd_minibatch_size'] = min(args.horizon * 10, 0.2 * config['train_batch_size'])
         else:
-            config['sgd_minibatch_size'] = 1280
+            config['sgd_minibatch_size'] = min(config['train_batch_size'], 10000)
         if args.use_lstm:
             config['vf_loss_coeff'] = args.vf_loss_coeff
             # Grid search things
         if args.grid_search:
             config['lr'] = tune.grid_search([5e-5, 5e-4])
         else:
-            config['num_sgd_iter'] = 30
+            config['num_sgd_iter'] = 10
 
             # LSTM Things
         if args.use_lstm and args.use_gru:
