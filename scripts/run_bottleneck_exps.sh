475fe94075221905d0d173ad54d7a765cf5412c8
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-14 22:32:54 -0700

    ray version to 0.6.3

--- scripts/run_bottleneck_exps.sh
+++ scripts/run_bottleneck_exps.sh
@@ -3,4 +3,9 @@
 ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiDecentralObsBottleneck\
  --num_cpus 14 --use_s3 \
 --grid_search --centralized_vf --multi_node" \
---start --stop --cluster-name=ev_test1
+--start --stop --cluster-name=ev_test1 --tmux
+
+ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiCentralObsBottleneck\
+ --num_cpus 14 --use_s3 \
+--grid_search --centralized_vf --multi_node --central_obs" \
+--start --stop --cluster-name=ev_test2 --tmux
