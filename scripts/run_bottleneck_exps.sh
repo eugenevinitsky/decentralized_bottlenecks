dfc70d855cec75b9de4c30f7370592de596be4c5
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-14 21:30:12 -0700

    Minor

--- scripts/run_bottleneck_exps.sh
+++ scripts/run_bottleneck_exps.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ray exec "python flow/examples/rllib/multiagent_exps" --start --stop --cluster-name=ev_test1 --tmux
+ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiDecentralObsBottleneck\
+ --num_cpus 14 --use_s3 \
+--grid_search --centralized_vf --multi_node" \
+--start --stop --cluster-name=ev_test1
