2dd0dde63529ee89cd75c96ef3d2f17dd3431850
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-15 23:51:14 -0700

    Testing just a high inflow

--- scripts/run_bottleneck_exps.sh
+++ scripts/run_bottleneck_exps.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+#ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiDecentralObsBottleneck\
+# --num_cpus 14 --use_s3 \
+#--grid_search --multi_node --rollout_scale_factor 2 --num_samples 2" \
+#--start --stop --cluster-name=ev_test1 --tmux
+#
+#ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiCentralObsBottleneck\
+# --num_cpus 14 --use_s3 \
+#--grid_search --multi_node --central_obs --rollout_scale_factor 2 --num_samples 2" \
+#--start --stop --cluster-name=ev_test2 --tmux
+
 ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiDecentralObsBottleneck\
  --num_cpus 14 --use_s3 \
---grid_search --multi_node --rollout_scale_factor 2 --num_samples 2" \
---start --stop --cluster-name=ev_test1 --tmux
+--grid_search --multi_node --rollout_scale_factor 2 --num_samples 2 --low_inflow 2400 --high_inflow 2400" \
+--start --stop --cluster-name=ev_test3 --tmux
 
 ray exec ray_autoscale.yaml "python flow/examples/rllib/multiagent_exps/multiagent_bottleneck.py MultiCentralObsBottleneck\
  --num_cpus 14 --use_s3 \
---grid_search --multi_node --central_obs --rollout_scale_factor 2 --num_samples 2" \
---start --stop --cluster-name=ev_test2 --tmux
+--grid_search --multi_node --central_obs --rollout_scale_factor 2 --num_samples 2 --low_inflow 2400 --high_inflow 2400" \
+--start --stop --cluster-name=ev_test4 --tmux
