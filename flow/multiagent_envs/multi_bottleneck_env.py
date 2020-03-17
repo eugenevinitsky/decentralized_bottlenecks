503182e0129caf4707f92a9e7b1d65edf7e34a1b
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-17 10:25:27 -0700

    Add outflow reward

--- flow/multiagent_envs/multi_bottleneck_env.py
+++ flow/multiagent_envs/multi_bottleneck_env.py
@@ -215,13 +215,15 @@ class MultiBottleneckEnv(MultiEnv, DesiredVelocityEnv):
                 return 0
 
         # if rl_actions:
-        reward = -1
+        reward = -2
+        reward += self.k.vehicle.get_outflow_rate(10) / 2000.0
         add_params = self.env_params.additional_params
         if add_params["congest_penalty"]:
             num_vehs = len(self.k.vehicle.get_ids_by_edge('4'))
             if num_vehs > 30*self.scaling:
                 penalty = (num_vehs - 30*self.scaling)/10.0
                 reward -= penalty
+
         return {rl_id: reward for rl_id in self.k.vehicle.get_rl_ids()}
         # else:
         #     return {}
