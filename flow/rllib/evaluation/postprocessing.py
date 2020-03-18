c0f0cbf2c0aa35a632c3f7b5c0a20e31d3caebe4
Eugene Vinitsky <vinitsky.eugene@gmail.com>
2020-03-18 14:17:05 -0700

    Swap out postprocessing

--- flow/rllib/evaluation/postprocessing.py
+++ flow/rllib/evaluation/postprocessing.py
@@ -13,16 +13,17 @@ def discount(x, gamma):
 
 
 @DeveloperAPI
-def compute_advantages(rollout, last_r, gamma=0.9, lambda_=1.0, use_gae=True):
+def compute_advantages(rollout, last_r, gamma=0.9, lambda_=1.0, use_gae=True,
+                       use_centralized_vf=False):
     """Given a rollout, compute its value targets and the advantage.
-
     Args:
         rollout (SampleBatch): SampleBatch of a single trajectory
         last_r (float): Value estimation for last observation
         gamma (float): Discount factor.
         lambda_ (float): Parameter for GAE
         use_gae (bool): Using Generalized Advantage Estamation
-
+        use_centralized_vf (bool): whether we should compute the advantages from a
+            centralized value function
     Returns:
         SampleBatch (SampleBatch): Object with experience from rollout and
             processed rewards.
@@ -37,11 +38,22 @@ def compute_advantages(rollout, last_r, gamma=0.9, lambda_=1.0, use_gae=True):
         assert "vf_preds" in rollout, "Values not found!"
         vpred_t = np.concatenate([rollout["vf_preds"], np.array([last_r])])
         delta_t = traj["rewards"] + gamma * vpred_t[1:] - vpred_t[:-1]
+        advantages = discount(delta_t, gamma * lambda_)
+
+        if use_centralized_vf:
+            assert "central_vf_preds" in rollout, "Central values not found!"
+            central_vpred_t = np.concatenate([rollout["vf_preds"], np.array([last_r])])
+            central_delta_t = traj["rewards"] + gamma * central_vpred_t[1:] - central_vpred_t[:-1]
+            central_advantages = discount(central_delta_t, gamma * lambda_)
+            traj["advantages"] = central_advantages
+            traj["central_value_targets"] = (
+                    central_advantages + traj["central_vf_preds"]).copy().astype(np.float32)
+        else:
+            traj["advantages"] = advantages
         # This formula for the advantage comes
         # "Generalized Advantage Estimation": https://arxiv.org/abs/1506.02438
-        traj["advantages"] = discount(delta_t, gamma * lambda_)
         traj["value_targets"] = (
-            traj["advantages"] + traj["vf_preds"]).copy().astype(np.float32)
+            advantages + traj["vf_preds"]).copy().astype(np.float32)
     else:
         rewards_plus_v = np.concatenate(
             [rollout["rewards"], np.array([last_r])])
