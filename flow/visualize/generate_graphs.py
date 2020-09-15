176a7dd64f98094c927086470be16689909194d2
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-09-15 10:29:44 +0200

    140m fake radar

--- flow/visualize/generate_graphs.py
+++ flow/visualize/generate_graphs.py
@@ -41,7 +41,7 @@ if __name__ == '__main__':
     exp_title = '/'.join([exp_title_lst[0]] + exp_title_lst[2:])
     exp_title = exp_title.replace('/', '_') + "_CP_" + str(cp)
     if penetration is not None:
-        exp_title += f'_PEN_{penetration}_RD_reducedradar140'
+        exp_title += f'_PEN_{penetration}_RD_reducedradar140fake'
 
     # download checkpoints from AWS
     try:
@@ -65,4 +65,4 @@ if __name__ == '__main__':
                             horizon=400, end_len=500, penetration=penetration)  
 
     aws_sync(output_path,
-            os.path.join("s3://nathan.experiments/trb_bottleneck_paper/graphs_random_final3/", exp_title))
+            os.path.join("s3://nathan.experiments/trb_bottleneck_paper/graphs_random_final4/", exp_title))
