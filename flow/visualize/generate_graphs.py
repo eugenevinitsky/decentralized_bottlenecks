432f2c5e6a0e999c9fe83db4f5c3808b04b62716
Nathan Lichtlé <nathan.lichtle@gmail.com>
2020-11-02 09:54:33 +0100

    add gen graph script

--- flow/visualize/generate_graphs.py
+++ flow/visualize/generate_graphs.py
@@ -23,57 +23,46 @@ OUTFLOW_MAX = 3600
 OUTFLOW_STEP = 100
 
 if __name__ == '__main__':
+    
+    
+    if len(sys.argv) <= 3:
+        print('usage: generate_graphs.py exp_cp_path cp_number [new_penetration_fraction]')
     exp_cp_path = sys.argv[1]
     cp = sys.argv[2]
+    if len(sys.argv) >= 4:
+        penetration = float(sys.argv[3])
+    else:
+        penetration = None  # keep penetration used during training
+
     print("exp cp path: ", exp_cp_path)
-    exp_title = exp_cp_path.replace('/', '_') + "_CHECKPOINT_" + str(cp)
+    print("cp: ", cp)
+    print("penetration: ", penetration)
+    exp_title_lst = exp_cp_path.split('/')
+    exp_title = '/'.join([exp_title_lst[0]] + exp_title_lst[2:])
+    exp_title = exp_title.replace('/', '_') + "_CP_" + str(cp)
+    if penetration is not None:
+        exp_title += f'_PEN_{penetration}'
 
     # download checkpoints from AWS
-    os.makedirs(os.path.expanduser("~/ray_results"))
+    try:
+        os.makedirs(os.path.expanduser("~/ray_results"))
+    except:
+        pass
     aws_sync('s3://nathan.experiments/trb_bottleneck_paper/' + exp_cp_path,
              os.path.expanduser("~/ray_results/trb_bottleneck_paper/" + exp_cp_path))
 
-    ray.init()
+    ray.init(num_cpus=35)
     
     output_path = os.path.join(os.path.expanduser('~/bottleneck_results'))
-    os.makedirs(output_path)
+    try:
+        os.makedirs(output_path)
+    except:
+        pass
 
     local_cp_path = os.path.join(os.path.expanduser("~/ray_results/trb_bottleneck_paper"), exp_cp_path)
     run_bottleneck_results(OUTFLOW_MIN, OUTFLOW_MAX, OUTFLOW_STEP, NUM_TEST_TRIALS, output_path, exp_title, local_cp_path,
                             gen_emission=False, render_mode='no_render', checkpoint_num=str(cp),
-                            horizon=400, end_len=500)  
+                            horizon=400, end_len=500, penetration=penetration)  
 
     aws_sync(output_path,
-            os.path.join("s3://nathan.experiments/trb_bottleneck_paper/seeds_graphs/", exp_title))
-
-
-
-    # DATE = "07-30-2020"
-
-    # for EXP_TITLE in EXP_TITLE_LIST:
-    #     # create output dir
-    #     output_path = os.path.join(os.path.expanduser('~/bottleneck_results'), EXP_TITLE)
-    #     if not os.path.exists(output_path):
-    #         try:
-    #             os.makedirs(output_path)
-    #         except OSError as exc:
-    #             if exc.errno != errno.EEXIST:
-    #                 raise
-
-    #     # for each grid search, find checkpoint 2000
-    #     for (dirpath, dirnames, filenames) in os.walk(os.path.expanduser("~/ray_results")):
-    #         if "checkpoint_2000" in dirpath and EXP_TITLE in dirpath: #dirpath.split('/')[-3] == EXP_TITLE:
-    #             print('FOUND CHECKPOINT {}'.format(dirpath))
-
-    #             # grab the experiment name
-    #             folder = os.path.dirname(dirpath)
-    #             tune_name = folder.split("/")[-1]
-    #             checkpoint_path = os.path.dirname(dirpath)
-
-    #             print('GENERATING GRAPHS')
-    #             run_bottleneck_results(OUTFLOW_MIN, OUTFLOW_MAX, OUTFLOW_STEP, NUM_TEST_TRIALS, output_path, EXP_TITLE.replace('/', '_'), checkpoint_path,
-    #                                     gen_emission=False, render_mode='no_render', checkpoint_num="2000",
-    #                                     horizon=400, end_len=500)
-
-    #             aws_sync(output_path,
-    #                     "s3://nathan.experiments/trb_bottleneck_paper/seed_graphs/{}/{}/{}".format(DATE, EXP_TITLE.replace('/', '_'), tune_name))
+            os.path.join("s3://nathan.experiments/trb_bottleneck_paper/graphs/", exp_title))
